a1d65e4f662d02c358d003a98d6953dc76b8bf66
MarioTravanca7 <mtravanca92@gmail.com>
2022-08-30 10:22:48 +0000

    second commit

--- 34-thymeleafdemo-bd-css-crud-m80/src/main/java/com/luv2code/springboot/thymeleafdemo/ThymeleafdemoApplication.java
+++ 34-thymeleafdemo-bd-css-crud-m80/src/main/java/com/luv2code/springboot/thymeleafdemo/ThymeleafdemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ThymeleafdemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Start!");
 		SpringApplication.run(ThymeleafdemoApplication.class, args);
 	}
 
